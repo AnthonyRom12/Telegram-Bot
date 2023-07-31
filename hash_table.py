03daed34fe484cd43c32a21d9ca101d8423275a6
AnthonyRom12 <106614976+AnthonyRom12@users.noreply.github.com>
2023-07-31 23:30:39 +0300

    Initial commit

---
+++ hash_table.py
@@ -0,0 +1,49 @@
+python_hash_table = {
+    'for': 'A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, '
+           'or a string).\n'
+           'This is less like the for keyword in other programming languages, and works more like an iterator method '
+           'as found in other object-orientated programming languages.\n'
+           'With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.',
+    'for_statement_example': 'fruits = ["apple", "banana", "cherry"]\n'
+                             'for x in fruits:\n'
+                             'print(x)',
+    'while': 'With the while loop we can execute a set of statements as long as a condition is true.',
+    'while_example': 'i = 1\n'
+                     'while i < 6:\n'
+                     'print(i)\n'
+                     'i += 1\n',
+    'tuple': 'Tuples are used to store multiple items in a single variable.\n'
+             'Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are '
+             'List, Set, and Dictionary, all with different qualities and usage.\n '
+             'A tuple is a collection which is ordered and unchangeable.\n'
+             'Tuples are written with round brackets.',
+    'def': 'A function is a block of code which only runs when it is called.\n'
+           'You can pass data, known as parameters, into a function.\n'
+           'A function can return data as a result.',
+    'list': 'Lists are used to store multiple items in a single variable.\n'
+            'Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are '
+            'Tuple, Set, and Dictionary, all with different qualities and usage.\n '
+            'Lists are created using square brackets',
+    'array': 'Note: Python does not have built-in support for Arrays, but Python Lists can be used instead.',
+    'generator': 'None',
+    'dictionary': 'Dictionaries are used to store data values in key:value pairs.\n'
+                  'A dictionary is a collection which is ordered*, changeable and do not allow duplicates.\n'
+                  'As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are '
+                  'unordered.\n '
+                  'Dictionaries are written with curly brackets, and have keys and values',
+    'lambda': 'A lambda function is a small anonymous function.\n'
+              'A lambda function can take any number of arguments, but can only have one expression.',
+    'Numbers': 'There are three numeric types in Python:\n'
+           '* int\n'
+           '* float\n'
+           '* complex\n'
+           'Variables of numeric types are created when you assign a value to them:',
+    'str': 'Strings in python are surrounded by either single quotation marks, or double quotation marks.'
+           '"hello" is the same as "hello".\n'
+           'You can display a string literal with the print() function:',
+    'bool': 'In programming you often need to know if an expression is True or False.\n'
+            'You can evaluate any expression in Python, and get one of two answers, True or False.\n'
+            'When you compare two values, the expression is evaluated and Python returns the Boolean answer:'
+}
+
+# print(python_hash_table.get('for'))
---
+++ main.py
@@ -0,0 +1,104 @@
+import logging
+import telegram
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, CallbackQueryHandler
+from access import API_TOKEN
+from hash_table import python_hash_table
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [
+            InlineKeyboardButton("Python", callback_data='1', ),
+            InlineKeyboardButton("Java", callback_data='2'),
+        ],
+        [InlineKeyboardButton("C++", callback_data=3)]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(chat_id=update.message.chat_id, reply_markup=reply_markup, text='Choose Language')
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+
+    await query.answer()
+    if query.data == '1':
+        await query.edit_message_text(text=f"Selected option: {query.data},\n Use follow command to get more"
+                                           f"information: (Without asteriks)\n "
+                                           "* for\n"
+                                           "* for_statement_example\n"
+                                           "* while\n"
+                                           "* while_example\n"
+                                           "* tuple\n"
+                                           "* def\n"
+                                           "* list\n"
+                                           "* array\n"
+                                           "* generator\n"
+                                           "* dictionary\n"
+                                           "* lambda\n"
+                                           "* Numbers\n"
+                                           "* str\n"
+                                           "* bool")
+    if query.data == '2':
+        await query.edit_message_text(text=f"Selected option: {query.data},\n Sorry, but updating still in progress!"
+                                           f" "f"Try other commands! ")
+
+    if query.data == '3':
+        await query.edit_message_text(text=f"Selected option: {query.data},\n Sorry, but updating still in progress! "
+                                           f"Try other commands! ")
+
+
+async def help_(updater: Update, context: ContextTypes.DEFAULT_TYPE):
+    await updater.message.reply_text("Use follow command to get more information: (Without asteriks)\n"
+                                     "* for\n"
+                                     "* for_statement_example\n"
+                                     "* while\n"
+                                     "* while_example\n"
+                                     "* tuple\n"
+                                     "* def\n"
+                                     "* list\n"
+                                     "* array\n"
+                                     "* generator\n"
+                                     "* dictionary\n"
+                                     "* lambda\n"
+                                     "* Numbers\n"
+                                     "* str\n"
+                                     "* bool")
+
+    # def reply(message):
+    #     message = python_hash_table.get(message)
+    #     print(message)
+
+
+async def message_(updater: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = updater.message.text
+    # print(msg)
+    ans = python_hash_table.get(msg)
+    if ans:
+        await updater.message.reply_text(ans)  # python_hash_table.get(msg)
+    else:
+        await updater.message.reply_text("Oops something went wrong, try again!")
+
+    # Echo functions for creating echo bot
+    # async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(API_TOKEN).build()
+
+    # echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help_)
+    button_handler = CallbackQueryHandler(button)
+
+    application.add_handler(button_handler)
+    application.add_handler(start_handler)
+    application.add_handler(help_handler)
+    # application.add_handler(echo_handler)
+    application.add_handler(MessageHandler(filters.TEXT, message_))
+
+    application.run_polling()
