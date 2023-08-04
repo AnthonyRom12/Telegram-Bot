c6426494d29dec32b7f57a3e4a00500e93e90f71
AnthonyRom12 <106614976+AnthonyRom12@users.noreply.github.com>
2023-08-04 13:37:24 +0300

    Inline Buttons data fixed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import telegram
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, CallbackQueryHandler
+from telegram.ext import CallbackContext, filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, CallbackQueryHandler
 from access import API_TOKEN
 from hash_table import python_hash_table
 
@@ -33,58 +33,87 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.message.chat_id, reply_markup=reply_markup, text='Choose Language')
 
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def button(update: Update, context: CallbackContext):
     query = update.callback_query
+    data = query.data
 
-    await query.answer()
-    if query.data == 'Python':
-        # pyth0n_button = [
-        #     [InlineKeyboardButton("for", callback_data='for'),
-        #      InlineKeyboardButton("while", callback_data='2'),
-        #      InlineKeyboardButton("def", callback_data='3'),
-        #      InlineKeyboardButton("generator", callback_data='4'),
-        #      InlineKeyboardButton("Numbers", callback_data='5')],
-        #     [InlineKeyboardButton("tuple", callback_data='6'),
-        #      InlineKeyboardButton("dictionary", callback_data='7'),
-        #      InlineKeyboardButton("lamda", callback_data='8')],
-        #     [InlineKeyboardButton("array", callback_data='9'),
-        #      InlineKeyboardButton("str", callback_data='10'),
-        #      InlineKeyboardButton("bool", callback_data='11')],
-        # ]
-        #
-        # markup_button = InlineKeyboardMarkup(pyth0n_button)
-        # await query.edit_message_text(text='Python', reply_markup=markup_button)
+    # await query.answer()
+    if data == 'Python':
+        python_button = [
+            [InlineKeyboardButton("for", callback_data='for'),
+             InlineKeyboardButton("while", callback_data='while'),
+             InlineKeyboardButton("def", callback_data='def'),
+             InlineKeyboardButton("generator", callback_data='generator'),
+             InlineKeyboardButton("Numbers", callback_data='Numbers')],
+            [InlineKeyboardButton("tuple", callback_data='tuple'),
+             InlineKeyboardButton("dictionary", callback_data='dictionary'),
+             InlineKeyboardButton("lambda", callback_data='lambda')],
+            [InlineKeyboardButton("array", callback_data='array'),
+             InlineKeyboardButton("str", callback_data='str'),
+             InlineKeyboardButton("bool", callback_data='bool')],
+            [InlineKeyboardButton("class", callback_data='class'),
+             InlineKeyboardButton("polymorphism", callback_data='polymorphism'),
+             InlineKeyboardButton("inheritance", callback_data='inheritance')]
+        ]
 
-        await query.edit_message_text(text=f"Selected option: {query.data},\n Use follow command to get more"
-                                           f"information: (Without asteriks)\n "
-                                           "* for\n"
-                                           "* for_statement_example\n"
-                                           "* while\n"
-                                           "* while_example\n"
-                                           "* tuple\n"
-                                           "* def\n"
-                                           "* list\n"
-                                           "* array\n"
-                                           "* generator\n"
-                                           "* dictionary\n"
-                                           "* lambda\n"
-                                           "* Numbers\n"
-                                           "* str\n"
-                                           "* bool"
-                                           "* class\n"
-                                           "* polymorphism\n"
-                                           "* inheritance")
-    if query.data == 'Java':
+        markup_button = InlineKeyboardMarkup(python_button)
+        await query.edit_message_text(text='Python', reply_markup=markup_button)
+
+    elif data == 'for':  # TODO: message too long
+        info = python_hash_table.get(data)
+        # await query.answer(text=info, show_alert=True)
+        await query.edit_message_text(text=info)
+    elif data == 'while':
+        info = python_hash_table.get('while')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'def':
+        info = python_hash_table.get('def')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'array':
+        info = python_hash_table.get('array')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'list':
+        info = python_hash_table.get('list')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'tuple':  # TODO: message too long
+        info = python_hash_table.get('tuple')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'generator':
+        info = python_hash_table.get('generator')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'dictionary':  # TODO: message too long
+        info = python_hash_table.get('dictionary')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'lambda':
+        info = python_hash_table.get('lambda')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'str':
+        info = python_hash_table.get('str')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'bool':  # TODO: message too long
+        info = python_hash_table.get('bool')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'class':  # TODO: message too long
+        info = python_hash_table.get('class')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'polymorphism':
+        info = python_hash_table.get('polymorphism')
+        await query.answer(text=info, show_alert=True)
+    elif data == 'inheritance':  # TODO: message too long
+        info = python_hash_table.get('inheritance')
+        await query.answer(text=info, show_alert=True)
+
+    if data == 'Java':
         await query.edit_message_text(text=f"Selected option: {query.data},\n Sorry, but updating still in progress!"
                                            f" "f"Try other commands! ")
 
-    if query.data == 'C++':
+    if data == 'C++':
         await query.edit_message_text(text=f"Selected option: {query.data},\n Sorry, but updating still in progress! "
                                            f"Try other commands! ")
 
 
 async def help_(updater: Update, context: ContextTypes.DEFAULT_TYPE):
-    await updater.message.reply_text("Use follow command to get more information: (Without asteriks)\n"
+    await updater.message.reply_text("Hi, Use one of the follow command to get more information: (Without asteriks)\n"
                                      "* for\n"
                                      "* for_statement_example\n"
                                      "* while\n"
